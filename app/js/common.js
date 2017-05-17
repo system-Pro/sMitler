2f549ceb5d67d6fe0700ab69bdf5bb25d61af445
systemPro <sendmyletterto@gmail.com>
2017-05-17 22:35:11 +0300

    lesson14

--- app/js/common.js
+++ app/js/common.js
@@ -78,7 +78,31 @@ $(function () {
 		items: 1,
 		smartSpeed: 700,
 		nav: false,
-		autoHeight: true
+		// autoHeight: true
+	});
+
+	$('.partners').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+
+		}
 	});
 
 	//E-mail Ajax Send
