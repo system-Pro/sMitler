6799b7e9c5e989275af6ec66b1942410b99ffb9e
systemPro <sendmyletterto@gmail.com>
2017-05-18 13:12:21 +0300

    lesson15

--- app/js/common.js
+++ app/js/common.js
@@ -105,6 +105,18 @@ $(function () {
 		}
 	});
 
+	$(window).scroll(function(){
+		if($(this).scrollTop() > $(this).height()){
+			$('.top').addClass('active');
+		}
+		else{
+			$('.top').removeClass('active');
+		}
+	});
+	$('.top').click(function(){
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+
 	//E-mail Ajax Send
 	$("form.callback").submit(function () { //Change
 		var th = $(this);
@@ -132,3 +144,8 @@ $(function () {
 	}
 
 });
+
+
+$(window).on('load', function(){
+	$('.preloader').delay(1000).fadeOut('slow');
+})
